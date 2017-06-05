0d0465a3e9bc321736b2dd569a1d5d50c8ee2e39
fabianmoronzirfas <fabian.moron.zirfas@gmail.com>
2017-06-05 11:24:00 +0200

    changes made some time ago

--- index.js
+++ index.js
@@ -5,11 +5,19 @@
 const Plotter = require('./lib/plotter');
 
 const plotter = new Plotter();
+plotter.calibrate();
 
-plotter.on('ready', function(obj) {
-  console.log(obj);
+plotter.on('calibrated', function() {
   console.log('Plotter is ready');
-
+  let coords = [
+    [10, 20], [1000, 4000], [50, 10], [1000, 5000]
+  ];
+  plotter.penUp(false);
+  plotter.goto(coords[0]);
+  plotter.penUp(true);
 });
 
-plotter.calibrate();
+plotter.on('done', function(str) {
+  console.log(str);
+});
+
